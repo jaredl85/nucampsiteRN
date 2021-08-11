440395f5d931552969fca9611ee032827daf160a
jaredl85 <jaredloveridge@gmail.com>
2021-08-11 11:46:39 -0700

    Added social sharing capabilities

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -8,7 +8,8 @@ import {
   Button,
   StyleSheet,
   Alert,
-  PanResponder
+  PanResponder,
+  Share
 } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
@@ -73,6 +74,16 @@ function RenderCampsite(props) {
     }
   })
 
+  const shareCampsite = (title, message, url) => {
+    Share.share({
+      title,
+      message: `${title} : ${message} : ${url}`,
+      url
+    }, {
+      dialogTitle: 'Share ' + title
+    });
+  }
+
   if (campsite) {
     return (
       <Animatable.View 
@@ -107,6 +118,14 @@ function RenderCampsite(props) {
             raised
             reverse
             onPress={() => props.onShowModal()}
+          />
+          <Icon
+            name={'share'}
+            type="font-awesome"
+            color="#5637DD"
+            raised
+            reverse
+            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
           />
         </View>
       </Card>
